31fd5c867430d322ede02e99bad55722d0c1a6c6
Waqas ur Rehman Kamboh <waqas.kamboh1987@hotmail.com>
2021-07-09 08:51:02 +0500

    Add Controllers

---
+++ BookStorePortal/Controllers/EmployeeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStorePortal.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public string EmployeePage()
+        {
+            return "This is Employee Page";
+        }
+    }
+}
---
+++ BookStorePortal/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStorePortal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string getName()
+        {
+            return "My Name is Waqas";
+        }
+        public string ContactUs()
+        {
+            return "Contact Us 03126668399";
+        }
+    }
+}
